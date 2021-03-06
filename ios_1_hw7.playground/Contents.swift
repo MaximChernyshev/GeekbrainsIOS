2a593d9dc04cbce49f5b9a1f9f6b7ed1542b3eee
Maxim Chernyshev <chernyshev.maxim.v@gmail.com>
2021-03-06 23:41:28 +0300

    hw7 full

--- ios_1_hw7.playground/Contents.swift
+++ ios_1_hw7.playground/Contents.swift
@@ -1,3 +1,81 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+enum StartEngineError: Error {
+    case noFuel
+    case noKey
+    case invalidTemp
+}
+
+enum AccelerateError: Error {
+    case engineIsOff
+}
+
+class Engine {
+    var fuelLevel: UInt
+    var keyInCar: Bool
+    var temperature: Double
+    
+    var engineStarted: Bool = false
+    
+    init(fuelLevel: UInt, keyInCar: Bool, temperature: Double) {
+        self.fuelLevel = fuelLevel
+        self.keyInCar = keyInCar
+        self.temperature = temperature
+    }
+    
+    func  start() -> (Bool?, StartEngineError?) {
+        guard self.fuelLevel > 0 else {
+            return (nil, StartEngineError.noFuel)
+        }
+        
+        guard self.keyInCar == true else {
+            return (nil, StartEngineError.noKey)
+        }
+        
+        guard self.temperature > 50 || self.temperature < 50 else {
+            return (nil, StartEngineError.invalidTemp)
+        }
+        
+        engineStarted = true
+        return (engineStarted, nil)
+    }
+    
+    func go() throws -> Bool {
+        guard self.engineStarted else {
+            throw AccelerateError.engineIsOff
+        }
+        print("Engine started")
+        return true
+    }
+}
+
+var engine1 = Engine(fuelLevel: 10, keyInCar: false, temperature: 26)
+var engine2 = Engine(fuelLevel: 0, keyInCar: true, temperature: 30)
+var engine3 = Engine(fuelLevel: 2, keyInCar: true, temperature: 12)
+
+engine1.start()
+do {
+    try engine1.go()
+} catch AccelerateError.engineIsOff {
+    print("Engine is off")
+} catch let error {
+    print(error.localizedDescription)
+}
+
+engine2.start()
+do {
+    try engine2.go()
+} catch AccelerateError.engineIsOff {
+    print("Engine is off")
+} catch let error {
+    print(error.localizedDescription)
+}
+
+engine3.start()
+do {
+    try engine3.go()
+} catch AccelerateError.engineIsOff {
+    print("Engine is off")
+} catch let error {
+    print(error.localizedDescription)
+}
